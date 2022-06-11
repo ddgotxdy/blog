36d80a4911a1c775ef94e6aae7993c0a3b32ed53
ddgo <1812805089@qq.com>
2022-06-11 20:53:11 +0800

    初始化配置

---
+++ blog-api/src/main/java/com/ddgotxdy/config/WebConfig.java
@@ -0,0 +1,22 @@
+package com.ddgotxdy.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @author: ddgo
+ * @dateTime: 2022/6/11 20:48
+ * @version: 1.0.0
+ * @description: 跨域配置
+ */
+@Configuration
+public class WebConfig  implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        //跨域配置，不可设置为*，不安全, 前后端分离项目，可能域名不一致
+        //本地测试 端口不一致 也算跨域
+        registry.addMapping("/**").allowedOrigins("http://localhost:8080");
+    }
+}
