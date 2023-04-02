4cf7a485632e8293365880bb783ee795778fec32
ddgo <1812805089@qq.com>
2023-04-03 03:19:49 +0800

    fix bug

--- blog-common/src/main/java/top/ddgotxdy/common/client/BlogArticleClient.java
+++ blog-common/src/main/java/top/ddgotxdy/common/client/BlogArticleClient.java
@@ -1,6 +1,7 @@
 package top.ddgotxdy.common.client;
 
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import top.ddgotxdy.common.model.PageResult;
@@ -11,6 +12,7 @@ import top.ddgotxdy.common.model.article.ArticleListDTO;
  * @author: ddgo
  * @description: 博客服务对外接口
  */
+@Component
 @FeignClient("BlogArticleClient")
 @RequestMapping("openfeign/article")
 public interface BlogArticleClient {
