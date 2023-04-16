22df518c30e225bca401e1c4f8c43bd461abbdc0
ddgo <1812805089@qq.com>
2023-04-16 16:17:49 +0800

    filter scope begin

---
+++ blog-api/src/main/java/top/ddgotxdy/api/filter/ScopeFilter.java
@@ -0,0 +1,37 @@
+package top.ddgotxdy.api.filter;
+
+import com.github.phantomthief.scope.Scope;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * @author: ddgo
+ * @description:
+ */
+@Order(0)
+@Component
+@Slf4j
+public class ScopeFilter extends OncePerRequestFilter {
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
+                                    FilterChain filterChain) throws ServletException, IOException {
+        // 开启Scope
+        Scope.beginScope();
+        try {
+            log.info("fliter sucess");
+            filterChain.doFilter(request, response);
+        } finally {
+            // 关闭Scope
+            Scope.endScope();
+        }
+    }
+}
--- blog-api/src/main/java/top/ddgotxdy/api/interceptor/DataInitInterceptor.java
+++ blog-api/src/main/java/top/ddgotxdy/api/interceptor/DataInitInterceptor.java
@@ -1,10 +1,7 @@
 package top.ddgotxdy.api.interceptor;
 
-import com.github.phantomthief.scope.Scope;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.lang.Nullable;
 import org.springframework.web.servlet.HandlerInterceptor;
-import org.springframework.web.servlet.ModelAndView;
 import top.ddgotxdy.api.scope.ContextScope;
 
 import javax.servlet.http.HttpServletRequest;
@@ -24,8 +21,6 @@ public class DataInitInterceptor implements HandlerInterceptor {
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-        // 开启scope
-        Scope.beginScope();
         log.info("DataInitInterceptor 初始化");
         // 1.请求uri
         String uri = request.getRequestURI();
@@ -41,12 +36,6 @@ public class DataInitInterceptor implements HandlerInterceptor {
         ContextScope.setUserId(userId);
         log.info("uri [{}] method [{}] ip [{}] userid [{}]", uri, method, ip, userId);
         return true;
-    }
-
-    @Override
-    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, @Nullable ModelAndView modelAndView) throws Exception {
-        // 关闭scope
-        Scope.endScope();
     }
 
     private String getIpAddress(HttpServletRequest request) {
